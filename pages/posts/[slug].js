2269bafff2e8162a9a4a9733bd318b7a66c7201a
rblue36 <rblue@live.co.uk>
2021-10-07 15:47:50 +0100

    fix4

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -11,7 +11,7 @@ import Spacer from "../../components/Spacer/Spacer";
 import WorkImages from "../../components/WorkImages/WorkImages";
 import YouTube from "react-youtube";
 import styles from "./WorkPage.module.css";
-import markdownToHtml from "../../lib/markdownToHtml.js";
+import markdownToHtml from "../../lib/markdownToHtml";
 
 export default function Post({ post }) {
 	const router = useRouter();
