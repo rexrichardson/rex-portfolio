516ea7037ba78cec96cdd1e8325a3df03bb39475
rblue36 <rblue@live.co.uk>
2023-11-13 15:13:56 +0000

    WorkItem optimization
    Adjusted image to have no layout shift

--- components/WorkItem/WorkItem.js
+++ components/WorkItem/WorkItem.js
@@ -6,13 +6,19 @@ import Image from "next/image";
 
 const WorkItem = (props) => {
   return (
-    <Link as={`/posts/${props.slug}`} href="/posts/[slug]">
-      <div className={`mb-2 ${styles.imagecontainer}`}>
+    <Link
+      as={`/posts/${props.slug}`}
+      href="/posts/[slug]"
+      className="w-full h-full"
+    >
+      <div
+        className={`mb-2 ${styles.imagecontainer} relative w-full h-56  bg-white`}
+      >
         <Image
-          className={styles.img}
           src={props.img}
-          width={1000}
-          height={200}
+          fill
+          className="relative rounded-lg object-cover"
+          alt={props.name}
         />
         <div className="absolute w-full h-full  bg-gradient-to-t from-black  to-transparent top-0 left-0 opacity-10 rounded-lg"></div>
       </div>
