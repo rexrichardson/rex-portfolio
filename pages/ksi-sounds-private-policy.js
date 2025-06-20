5ad2581b24a70db466ca3e240a16a39de6ec8cf5
rblue36 <rblue@live.co.uk>
2025-06-20 18:27:48 +0100

    added private policy

---
+++ pages/ksi-sounds-private-policy.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return (
+    <div
+      style={{
+        maxWidth: 700,
+        margin: "auto",
+        padding: 20,
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1 className="text-xl font-bold">KSI Sounds Privacy Policy</h1>
+      <p>Effective Date: 20th June 2025</p>
+      <p>
+        The KSI Sounds app does not collect, store, or share any personal
+        information or usage data.
+      </p>
+      <p>We respect your privacy and are committed to keeping it that way.</p>
+    </div>
+  );
+}
